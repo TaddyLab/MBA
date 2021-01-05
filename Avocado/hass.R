c697a7706630855b0663ef37a3dc47fe5b94fcd7
taddy <mataddy@amazon.com>
2021-01-04 20:29:11 -0800

    hassy

--- Avocado/hass.R
+++ Avocado/hass.R
@@ -1,19 +1,27 @@
 
 #Hass avocado panel example
-hass<-read.csv("hass.csv",stringsAsFactors=TRUE)
+hass <- read.csv("hass.csv",stringsAsFactors=TRUE)
 head(hass,3)
 
 hass$date <- as.Date(hass$date,format='%m/%d/%Y')
 hass<-hass[order(hass$region,hass$date),] 
 head(hass,3)
 
-tapply(c(1:5), c("a","a","b","b","c"), function(x) max(x))
+fitHass <- glm(log(units) ~ log(asp) + region, data=hass)
+coef(fitHass)["log(asp)"]
+
+hass$week <- factor(hass$date)
+fitHassDF <- glm(log(units) ~ log(asp) + region + week, data=hass)
+coef(fitHassDF)["log(asp)"]
+
+
+tapply(c(1:5), c("a","b","a","b","c"), function(x) max(x))
 
 hass$lag.asp <- unlist(tapply(hass$asp, hass$region, 
 					function(x) c(NA,x[-length(x)])))
 hass$lag.units <- unlist(tapply(hass$units, hass$region, 
 					function(x) c(NA,x[-length(x)])))
 
-fitHass <- glm(log(units) ~ log(asp) + log(lag.units) 
+fitHassLags <- glm(log(units) ~ log(lag.units) + log(asp)  
 							+ log(lag.asp) + region, data=hass)
-summary(fitHass)$coefficients[2:4,] #equiv reg coefs
+summary(fitHassLags)$coefficients[2:4,] #equiv reg coefs
