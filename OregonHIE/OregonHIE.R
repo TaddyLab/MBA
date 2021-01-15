db5fd62380b1de4221fcea8f88f63de1c59a91a0
taddy <mataddy@amazon.com>
2021-01-15 14:16:07 -0800

    done oregon

--- OregonHIE/OregonHIE.R
+++ OregonHIE/OregonHIE.R
@@ -29,6 +29,9 @@ library(parallel)
 library(boot)
 ( bootATE <- boot(ohie, getATE, 1000, v="doc_num", parallel="snow", ncpus=detectCores() ) )
 
+## function for de-biased bootstrap confidence intervals
+getCI <- function(bo, p=c(.025,.975)) quantile(2*bo$t0 - bo$t, p)
+getCI(bootATE)
 
 nSel <- table(ohie$selected)
 yVar <- tapply(ohie$doc_num, ohie$selected, var)
@@ -49,24 +52,9 @@ fitLin <- glm(doc_num ~ selected + numhh, data=ohie)
 
 ## instead do this:
 fitAdj <- glm(doc_num ~ selected*numhh, data=ohie)
-( beta <- coef(fitAdj) )
-
-numhhMean <- table(ohie$numhh)/nrow(ohie)
-beta["selected"] + beta["selected:numhh2"]*numhhMean["2"] + beta["selected:numhh3+"]*numhhMean["3+"] 
-
-as.data.frame(table(ohie[,c("selected","numhh")]))
-as.data.frame(table(ohie[,c("numhh")]))
-
-unique(ohie[,c("selected","numhh")])
-table(ohie[,c("selected","numhh")])
-
 mean( 
 	predict(fitAdj, newdata=data.frame(selected=1, numhh=ohie$numhh)) - 
 	predict(fitAdj, newdata=data.frame(selected=0, numhh=ohie$numhh)) )
-
-x <- scale(model.matrix( ~ numhh, data=ohie)[,-1], scale=FALSE)
-colMeans(x)
-glm(doc_num ~ selected*x, data=ohie)
 
 getAdjATE <- function(data, ind){
 	fit <- glm(doc_num ~ selected*numhh, data=data[ind,])
@@ -76,12 +64,21 @@ getAdjATE <- function(data, ind){
 	predict(fit, newdata=data.frame(selected=0, numhh=data$numhh[ind])) )
 }
 getAdjATE(ohie,1:nrow(ohie))
-
 ( bootAdjATE <- boot(ohie, getAdjATE, 1000, parallel="snow", ncpus=detectCores() ) )
+getCI(bootAdjATE)
 
+## maybe faster, but only works for linear models:
+xshift <- scale(model.matrix( ~ numhh, data=ohie)[,-1], scale=FALSE)
+colMeans(xshift)
+coef(fitxshift <- glm(doc_num ~ selected*xshift, data=ohie))["selected"]
+#### in this case, the bootstrap SE and CLT SE are similar
+summary(fitxshift )
+( bootAdjATE )
+
+## look at conditional dependence within households.
 ohieHH <- split(ohie, ohie$household)
 length(ohieHH)
-library(data.table)
+library(data.table) # way faster!
 system.time(a <- rbindlist(ohieHH))
 system.time(b <- do.call("rbind",ohieHH))
 all(a$doc_num==b$doc_num)
@@ -93,7 +90,8 @@ getBlock <- function(data, ids, fun, binder){
 system.time( getBlock(ohieHH, 1:length(ohieHH), fun=getAdjATE, binder=rbindlist) )
 ( bootATEblock <- boot(ohieHH, getBlock, fun=getAdjATE, binder=rbindlist,
                           1000, parallel="snow", ncpus=detectCores()) )
-
+## get the quantile interval
+getCI(bootATEblock)
 
 ## same ideas but logit
 getAdjATE_any <- function(data, ind){
@@ -107,46 +105,31 @@ getAdjATE_any <- function(data, ind){
 getAdjATE_any(ohie,1:nrow(ohie))
 
 ( bootAdjATE_any <- boot(ohie, getAdjATE_any, 1000, parallel="snow", ncpus=detectCores() ) )
+getCI(bootAdjATE_any)
 
 #### OHIE 2SLS
 stage1 <- glm( medicaid ~ selected + numhh, data=ohie)
 pHat <- predict(stage1, newdata=ohie)
-stage2 <- glm( doc_num ~ pHat*numhh, data=ohie)
-summary(stage2)
+stage2 <- glm( doc_num ~ pHat + numhh, data=ohie)
+coef(stage2)["pHat"]
 
 getIV <- function(data, ind){
-	stage1 <- glm( medicaid ~ selected + numhh, data=ohie)
-	pHat <- predict(stage1, newdata=ohie)
-	stage2 <- glm( doc_num ~ pHat + numhh, data=ohie)
-	mean( 
-	predict(fit, newdata=data.frame(selected=1, numhh=data$numhh), type="response") - 
-	predict(fit, newdata=data.frame(selected=0, numhh=data$numhh), type="response") )
+	data <- data[ind,]
+	stage1 <- glm( medicaid ~ selected + numhh, data=data)
+	pHat <- predict(stage1, newdata=data)
+	stage2 <- glm( doc_num ~ pHat + numhh, data=data)
+	coef(stage2)["pHat"]
 }
-getAdjATE_any(ohie,1:nrow(ohie))
+( bootIV <- boot(ohie, getIV, 1000, parallel="snow", ncpus=detectCores() ) )
+( bootIVblock <- boot(ohieHH, getBlock, fun=getIV, binder=rbindlist, 
+						1000, parallel="snow", ncpus=detectCores() ) )
 
-
-# SEs using sandwich with complicated meat
-library(Matrix)
-resids <- P$doc_any - predict( stage2,
-     newdata=data.frame(numhh=P$numhh, pHat=P$medicaid))
-meat <- Diagonal(x=resids^2)
-bread <- stage2$x%*%solve(t(stage2$x)%*%stage2$x)
-sandwich <- t(bread)%*%meat%*%bread
-print( segam <- sqrt(sandwich[2,2]) )
-
-# 95% interval
-coef(stage2)["pHat"] + c(-2,2)*segam
-
-# SEs using AER package 
+# IV and SEs using the AER package
 library(AER)
-aerIV <- ivreg( doc_any  ~ medicaid + numhh | selected + numhh, data=P)
+aerIV <- ivreg( doc_num  ~ medicaid + numhh | selected + numhh, data=ohie)
 summary(aerIV)
 
-# closer to sandwich result
-sqrt(vcovHC(aerIV)[2,2])
-
-# cluster by household
-(seClust <- sqrt(vcovCL(aerIV, cluster = P$household_id)[2,2]) )
-
-coef(aerIV)["medicaid"] + c(-2,2)*seClust
+library(lmtest)
+VCL = vcovCL(aerIV, ohie$household) 
+coeftest(aerIV, vcov = VCL)
 
