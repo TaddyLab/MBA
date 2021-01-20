6c9bbd19c255c8161c9cfdd852ed68c6051259bc
taddy <mataddy@amazon.com>
2021-01-19 23:37:51 -0800

    done RD

--- RD/RD.R
+++ RD/RD.R
@@ -1,38 +1,28 @@
 
-D <- read.csv("RD.csv")
+dat <- read.csv("RD.csv")
 
 par(mai=c(.8,.8,.3,.3))
-boxplot(score ~ treat, data=D, horizontal=TRUE, 
+boxplot(score ~ treat, data=dat, horizontal=TRUE, 
 	xlab="rank score minus reserve",
  	ylab="treatment (Ad in Main)")
 abline(v=0, col=8, lty=3)
 
+h <- 3
+window <- which(dat$score > -h & dat$score < h)
+summary(linfit <- glm(y ~ treat*score, data=dat, subset=window))
 
+# difference in means
+summary( meandiff <- glm( y ~ treat, data=dat, subset=window) )
 
 # a neighborhood
 w <- 3
-above <- which(D$score > 0 & D$score <w)
-below <- which(D$score < 0 & D$score >-w)
-
-# constant model
-mua <- mean(D$y[above])
-mub <- mean(D$y[below])
-(te <- mua - mub)
-vara <- var(D$y[above])
-varb <- var(D$y[below])
-sdte <- sqrt(vara/length(above) + varb/length(below))
-te + c(-2,2)*sdte
-
-# local linear regression
-fita <- loess(y ~ score, data=D[above,], degree=1)
-fitb <- loess(y ~ score, data=D[below,], degree=1)
+above <- which(dat$score > 0 & dat$score <w)
+below <- which(dat$score < 0 & dat$score >-w)
+fita <- loess(y ~ score, data=dat[above,], degree=1)
+fitb <- loess(y ~ score, data=dat[below,], degree=1)
 rr <- seq(0.001,w-0.001,length=100)
 preda <- predict(fita,rr) 
 predb <- predict(fitb,-rr)
-
-h <- 3
-window <- which(D$score > -h & D$score < h)
-summary(linfit <- lm(y ~ treat*score, data=D, subset=window))
 
 nr <- 10
 rgrid <- seq(0, h, length=nr)
@@ -40,18 +30,22 @@ dgrid <- data.frame(score=c(-rev(rgrid),rgrid),
 					treat=rep(c(0,1),each=nr))
 linpred <- predict(linfit, newdata=dgrid)
 
+## alternatively, HC standard errors.
+library(sandwich)
+library(lmtest)
+coeftest(linfit, vcov=vcovHC(linfit))
 
-library(AER)
-( ate <- coef(linfit)["treat"] )
-seate <- sqrt(vcovHC(linfit)["treat","treat"])
-ate + c(-2,2)*seate
 
+png('RDanalysisData.png', width=4, height=4, units="in", res=720)
+plot(y ~ score, data=dat, subset=sample(c(above,below),10000), 
+		cex=.3, col=8, bty="n", xlab="r", main="")
+dev.off()
 
-par(mfrow=c(1,2), omi=c(.5,0,0,0), mai=c(.3,.9,.4,.2))
-plot(y ~ score, data=D, subset=sample(c(above,below),10000), 
-		cex=.3, col=8, bty="n", xlab="", main="data sample")
-plot(rr, preda, xlab="", col="grey50", ylab="y",lwd=2, 
-	main="RD analysis",
+mub <- coef(meandiff)[1]
+mua <- coef(meandiff)[2]
+png('RDanalysisZoomed.png', width=4, height=4, units="in", res=720)
+plot(rr, preda, xlab="r", col="grey50", ylab="y",lwd=2, 
+	main="",
 	ylim=range(c(preda,predb)), xlim=c(-w,w), type="l", bty="n")
 legend("right", bty="n", lwd=2, lty=c(1,2,1), col=c("grey50","red","blue"), 
 	legend=c("loess","constant","linear"))
@@ -61,16 +55,15 @@ lines(dgrid$score[nr+1:nr], linpred[nr+1:nr], lwd=1.5, col=4)
 lines(dgrid$score[1:nr], rep(mub,nr), lwd=1.5, lty=2, col=2)
 lines(dgrid$score[nr+1:nr], rep(mua,nr), lwd=1.5, lty=2, col=2)
 lines(c(0,0),coef(linfit)[1] + c(0,coef(linfit)[2]), lwd=1.5, col=2, lty=3)
-mtext(side=1, "rank score", outer=TRUE, line=1)
-
+dev.off()
 
 
 hh <- seq(.1,5,length=50)
 ateh <- seah <- rep(0,length(hh))
 for(i in 1:length(hh)){
 	print(i)
-	fith <- lm(y ~ treat*score, data=D, 
-		subset=which(abs(D$score) < hh[i]))
+	fith <- lm(y ~ treat*score, data=dat, 
+		subset=which(abs(dat$score) < hh[i]))
 	ateh[i] <- coef(fith)[2]
 	seah[i] <- sqrt(vcovHC(fith)[2,2])
 }
