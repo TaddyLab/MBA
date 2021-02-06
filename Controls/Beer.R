b11bd48d5d1c3d62c355333dd2b696c1ea452f66
taddy <mataddy@amazon.com>
2021-02-05 23:56:11 -0800

    through HTE

--- Controls/Beer.R
+++ Controls/Beer.R
@@ -7,26 +7,26 @@ head(sales)
 head(upc)
 
 # how many upcs?
-length( upctab <- table(sales$upc) )
 dim(upc)
 
 # check data types
 str(sales)
 
-# create priceper12oz
-sales$lpoz <- log(sales$price/upc[as.character(sales$upc),"oz"])
-
 # calculate lags
-sales <- sales[order(sales$store,sales$upc,sales$week),] 
-sales$lag <- unlist(tapply(sales$units, list(sales$upc,sales$store), 
-					function(x) c(NA,x[-length(x)])))
+sales <- sales[order(sales$upc,sales$week),] 
+sales$lag <- unlist(tapply(sales$units, sales$upc, 
+							function(x) c(NA,x[-length(x)])))
 sales <- sales[!is.na(sales$lag),]
 head(sales)
 tail(sales)
 
+# create price per oz
+sales$lpoz <- log(sales$price/upc[as.character(sales$upc),"oz"])
+
 ## calculate fixed effects
 library(gamlr)
-x <- sparse.model.matrix( ~ store + week + upc, data=sales)[,-1]
+x <- sparse.model.matrix( ~ week + upc, data=sales)[,-1]
+dim(x)
 
 # parse the item description text as a bag o' words
 library(tm)
@@ -36,9 +36,10 @@ w <- sparseMatrix(i=w$i,j=w$j,x=as.numeric(w$v>0), # convert from stm to Matrix 
               dims=dim(w),dimnames=list(rownames(upc),colnames(w)))
 
 w[1:5,1:6]
-w[287,w[287,]!=0]
+w[242,w[242,]!=0]
 dim(w)
 ## match to the observations
+wupc <- w
 w <- w[as.character(sales$upc),]
 dim(w)
 
@@ -52,8 +53,10 @@ naivefit <- gamlr(cbind(lpoz=sales$lpoz,llag=log(sales$lag), x),
 coef(naivefit)[2:3,]
 
 ## LTE lasso
-dfit <- gamlr(x,sales$lpoz, standardize=FALSE, lmr=1e-6)
+dfit <- gamlr(x,sales$lpoz, standardize=FALSE, lmr=1e-5)
 dhat <- drop(predict(dfit, x))
+resid <- sales$lpoz - dhat
+
 
 png('beerTreatLasso.png', width=4, height=5, units="in", res=720)
 plot(dfit)
@@ -63,63 +66,33 @@ plot(dhat[1:1e5] ~ sales$lpoz[1:1e5],
 	col="purple", cex=.5, pch=21, bty="n", xlab="d (lpoz)", ylab="dhat")
 dev.off()
 
-# if you try to fit with OLS its too big in dense format
-# > olsfit <- glm( log(units) ~ lpoz + as.matrix(xfe), data=sales )
-# Error: cannot allocate vector of size 7.8 Gb
 
-fullfit <- gamlr(cbind(lpos=sales$lpoz,llag=log(sales$lag), xfe), 
-			     log(sales$units), lambda.start=0)
-coef(fullfit)[2:3,]
+beerATE <- gamlr(cbind(lpoz=resid,llag=log(sales$lag), x), 
+			     log(sales$units), free=1:2, standardize=FALSE, lmr=1e-4)
+coef(beerATE)[2:3,]
 
-# grab a small subsample
-# naive lasso
-set.seed(888)
-ss <- sample.int(nrow(sales),5000)
-naivefit <- gamlr(cbind(llag=log(sales$lag), lpos=sales$lpoz, xfe)[ss,], 
-			      log(sales$units)[ss], free=1:2, standardize=FALSE, lmr=1e-5)
-print( coef(naivefit)[2:3,] )
+# fit with OLS 
+beerOLS <- glm( log(units) ~ lpoz + log(lag) + as.matrix(x), data=sales )
+summary(beerOLS)
 
-# ols fit
-olsfit <- gamlr(cbind(llag=log(sales$lag), lpos=sales$lpoz, xfe)[ss,], 
-			      log(sales$units)[ss], lambda.start=0)
-print( coef(olsfit)[2:3,] )
+# do the HTE fit.
 
-# orthogonal ML
-omlfit <- orthoML(x=xfe[ss,], 
-				d=cbind(llag=log(sales$lag), lpos=sales$lpoz)[ss,], 
-				y=log(sales$units)[ss],
-				standardize=TRUE, nfold=5, lmr=1e-5)
-summary(omlfit)
+nuw <- cbind(lpoz=resid, resid*w)
+beerHTE <- gamlr(cbind(nuw, llag=log(sales$lag), x), 
+			 log(sales$units), free="llag", standardize=FALSE, lmr=1e-3)
 
-# residual fit
-dfit <- gamlr(xfe[ss,], sales$lpoz[ss], standardize=FALSE, lmr=1e-3)
-dtil <- drop(sales$lpoz - predict(dfit, xfe))
-resfit <- gamlr(cbind(llag=log(sales$lag), lpos=dtil, xfe)[ss,], 
-			      log(sales$units)[ss], standardize=FALSE, lmr=1e-3, free=1)
-print( coef(resfit)[2:3,] )
+gam <- coef(beerHTE)[c("lpoz",colnames(w)),]
+head(gam)
 
-##############  heterogeneity
-## OLS just labs
-u <- sparse.model.matrix( ~ upc, data=sales)[,-1]
-llag <- log(sales$lag)
-htefe <- gamlr(cbind(llag=llag, lpos=dtil, u*dtil, xfe), 
-			      log(sales$units), standardize=FALSE, free=1, lmr=1e-6)
-coef(htefe)[1:4,]
-gammafe <- coef(htefe)[3] + drop(u%*%coef(htefe)[4:(ncol(u)+3),])
-hist(gammafe, freq=FALSE, xlab="elastcity", main="UPC Fixed Effects")
+elastics <- drop( cbind(1,wupc) %*% gam )			
+upc$elastics <- elastics
+head(upc,1)
+tail(upc,1)
 
+png('beerElastics.png', width=5, height=5, units="in", res=720)
+hist(elastics, freq=FALSE, xlab="elastcity", main="UPC elasticities")
+dev.off()
 
-# double ML
-hteres <- gamlr(cbind(llag=llag, lpos=dtil, w*dtil, xfe), 
-			      log(sales$units), standardize=FALSE, free=1, lmr=1e-6)
-coef(hteres)[1:4,]
-gammares <- coef(hteres)[3] + drop(w%*%coef(hteres)[4:(ncol(w)+3),])
-hist(gammares, freq=FALSE, xlab="elastcity", main="UPC Text Effects")
-#plot(gammares,gammafe)
+upc[which.min(elastics),]
+upc[which.max(elastics),]
 
-B <- coef(hteres)[4:(ncol(w)+3),]
-B <- B[B!=0]
-head(sort(round(B,2)))
-head(sort(round(B,2), decreasing=TRUE))
-
-upc[rownames(upc)%in% as.character(sales$upc[which(gammares>0)]),]
