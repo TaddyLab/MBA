0cc1148fcb38b525d8cd3ed398b372ce1ec86268
taddy <mataddy@amazon.com>
2021-02-26 00:00:28 -0800

    all done yelp

--- Yelp/textAsData.R
+++ Yelp/textAsData.R
@@ -10,54 +10,43 @@ reviews <- stream_in(file("Yelp60kReviews.json"))
 dim(reviews)
 reviews[7,]
 
-library(stopwords)
-stopWords <- stopwords(language="en",source = "snowball") #English language, snowball list
-
-# if you want to keep the words "above", and "below", edit the command:
-stopWords<-stopWords[!stopWords%in%c("above","below")]
-
 # create a doc term matrix
 library(text2vec) # see http://text2vec.org/
 library(Matrix)  ## always load this when you load text2vec
-prep = tolower  #make everything lowercase
-tok = word_tokenizer #words are the tokens
-# prep
-itYelp = itoken(reviews$text, 
-             preprocessor = prep, 
-             tokenizer = tok, 
+tokYelp = itoken(reviews$text, 
+             preprocessor = tolower, 
+             tokenizer = word_tokenizer, 
              ids = reviews$review_id)
 
-# create the vocab
-vocab<-create_vocabulary(itYelp, stopwords=stopWords)
+# create the vocab and prune stopwords
+vocab<-create_vocabulary(tokYelp, stopword=c("if","and","or"))
 
 # prune vocab with settings for rare and common words
 vocab<-prune_vocabulary(vocab,
-            term_count_min = 10,
-            doc_proportion_max = 0.5,
+            doc_proportion_max = 0.1,
             doc_proportion_min = 0.001)
 
 vectorizer = vocab_vectorizer(vocab)
-dtm = create_dtm(itYelp, vectorizer)
+dtm = create_dtm(tokYelp, vectorizer)
 class(dtm)
 dim(dtm)
-head(colnames(dtm))
 
 # look at column counts for the bag of words
 colSums(dtm[,1:9])
-colSums(dtm[,4600:4609])
-
+colSums(dtm[,ncol(dtm)-0:3]>0)
+dtm[1,dtm[1,]!=0]
 
 # we can get bigrams (2-grams) instead of single words with the "ngrams" argument
 # ngrams=c(1,2) uses single words and word pairs
 # ngrams=c(1,3) used single words, bigrams, and 3-grams (three adjacent words)
-vocab2<-create_vocabulary(itYelp,stopwords=stopWords,ngram=c(1,2))
+vocab2<-create_vocabulary(tokYelp,ngram=c(1,2))
 vocab2<-prune_vocabulary(vocab2,
-            term_count_min = 10,
-            doc_proportion_max = 0.5,
+            doc_proportion_max = 0.1,
             doc_proportion_min = 0.001)
 vectorizer2 = vocab_vectorizer(vocab2)
-dtm2 = create_dtm(itYelp, vectorizer2)
+dtm2 = create_dtm(tokYelp, vectorizer2)
 dim(dtm2)
+dtm2[1,dtm2[1,]>0]
 head(colnames(dtm2))
 tail(colnames(dtm2))
 
@@ -66,25 +55,23 @@ stem_tokenizer =function(x) {
    tokens = word_tokenizer(x)
    lapply(tokens, SnowballC::wordStem, language="en")
 }
-itStemYelp <- itoken(reviews$text, 
-          preprocessor = prep, 
+tokStemYelp <- itoken(reviews$text, 
+          preprocessor = tolower, 
           tokenizer = stem_tokenizer, 
           ids = reviews$id)
-vocabS <- create_vocabulary(itStemYelp,stopwords=stopWords) 
+vocabS <- create_vocabulary(tokStemYelp) 
 vocabS <- prune_vocabulary(vocabS,
-            term_count_min = 10,
-            doc_proportion_max = 0.5,
+            doc_proportion_max = 0.1,
             doc_proportion_min = 0.001)
 vectorizerS = vocab_vectorizer(vocabS)
-dtmS = create_dtm(itStemYelp, vectorizerS)
+dtmS = create_dtm(tokStemYelp, vectorizerS)
 dim(dtmS)
-head(colnames(dtmS))
+dtmS[1,dtmS[1,]>0]
 
 ### Text regression
 
 # lasso linear regression
 library(gamlr)
-
 fitlin <- gamlr(dtm>0, reviews$stars, lmr=1e-3)
 
 png('yelpLinpath.png', width=4.5, height=4.5, units="in", res=720)
@@ -101,6 +88,7 @@ dev.off()
 ## the most positive and negative short reviews
 l <- rowSums(dtm)
 reviews$text[l<10][which.max(yhat[l<10])]
+
 reviews$text[l<10][which.min(yhat[l<10])]
 
 ## inspect coefficients if you'd like
@@ -121,8 +109,8 @@ for(k in names(fitdmr)) plot(fitdmr[[k]], main=k)
 
 phat <- predict(fitdmr, dtm>0, type="response")
 
-png('yelpDMRfit.png', width=4.5, height=4.5, units="in", res=720)
-boxplot(phat[cbind(1:nrow(phat),reviews$stars)] ~ stars, data=reviews, 
+png('yelpDMRfit.png', width=4, height=6, units="in", res=720)
+boxplot(phat[cbind(1:nrow(phat),reviews$stars)] ~ stars, ylim=c(0,1), data=reviews, 
         xlab="star rating", ylab="p.hat for true rating", col=rev(heat.colors(5)))
 dev.off()
 
@@ -132,7 +120,7 @@ reviews$text[l<10][which.max(phat[l<10,5])]
 
 ## coefficients
 B <- coef(fitdmr)
-head(B)
+head(round(B,2))
 B["sucks",]
 exp( B["sucks",1] - B["sucks",5] )
 exp( B["sucks",1] - B["sucks",2] )
@@ -147,19 +135,34 @@ tpc <- LDA$new(n_topics = 20)
 W <- tpc$fit_transform(dtm)
 
 dim(W)
-round(head(W),2)
-head(rowSums(W))
+round(W[1,],2)
+sum(W[1,])
+
+drop( tpc$get_top_words(n = 5, topic=7, lambda=1) )
+drop( tpc$get_top_words(n = 5, topic=7, lambda=0) )
+drop( tpc$get_top_words(n = 5, topic=19, lambda=1) )
+drop( tpc$get_top_words(n = 5, topic=19, lambda=0) )
 
 # install.packages(c("LDAvis","servr"))
 tpc$plot()
 
 # topic regression
 library(ranger)
+wdat <- data.frame(stars=factor(reviews$stars),W)
+topicRF <- ranger(stars ~ ., data=wdat, num.tree=100, probability=TRUE)
+## misclassification rate
+topicRF$prediction.error
 
-wagerf <- ranger(factor(reviews$stars) ~ W, data=reviews, num.tree=100)
+pwRF <- predict(topicRF, wdat)$predictions
+round(pwRF[1,],2)
+
+png('yelpTopicRF.png', width=4, height=6, units="in", res=720)
+boxplot(pwRF[cbind(1:nrow(reviews),reviews$stars)] ~ stars, ylim=c(0,1), data=reviews, 
+        xlab="star rating", ylab="p.hat for true rating", col=rev(heat.colors(5)))
+dev.off()
 
 ### WORD EMBEDDING
-tcm = create_tcm(itYelp, vectorizer)
+tcm = create_tcm(tokYelp, vectorizer)
 class(tcm)
 dim(tcm)
 mean(tcm==0)
@@ -169,12 +172,12 @@ wGlove = glove$fit_transform(tcm)
 dim(wGlove)
 
 sims = sim2(x = wGlove, y = wGlove[c("sucks","upscale"),])
-sort(sims[,"sucks"], decreasing = TRUE)[1:10]
-sort(sims[,"upscale"], decreasing = TRUE)[1:10]
+sort(sims[,"sucks"], decreasing = TRUE)[1:5]
+sort(sims[,"upscale"], decreasing = TRUE)[1:5]
 
-taco <- wGlove["cheese",] - wGlove["pizza",] + wGlove["taco",]
+taco <- wGlove["pizza",] - wGlove["pepperoni",] + wGlove["tofu",]
 sims <- sim2(x = wGlove, y = matrix(taco,nrow=1))
-sort(sims[,1], decreasing = TRUE)[1:10]
+sort(sims[,1], decreasing = TRUE)[1:5]
 
 ## 
 all(rownames(wGlove)==colnames(dtm))
@@ -184,6 +187,18 @@ length(l)
 
 # create the review vectors as averages of word vectors for each review
 V = as.matrix( (dtm %*% wGlove)/l )
-head(V)
-summary(glm(reviews$stars ~ V))
+V[1,]
+V[is.na(V)] <- 0
 
+# random forest
+vdat <- data.frame(stars=factor(reviews$stars),V)
+gloveRF <- ranger(stars ~ ., data=vdat, num.tree=100, probability=TRUE)
+## misclassification rate
+gloveRF$prediction.error
+
+pvRF <- predict(gloveRF, vdat)$predictions
+
+png('yelpGloveRF.png', width=4, height=6, units="in", res=720)
+boxplot(pvRF[cbind(1:nrow(reviews),reviews$stars)] ~ stars, ylim=c(0,1), data=reviews, 
+        xlab="star rating", ylab="p.hat for true rating", col=rev(heat.colors(5)))
+dev.off()
